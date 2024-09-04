665c85130d17426aa3e594448d9a7b5c390e3ce7
elhamod <55500282+elhamod@users.noreply.github.com>
2024-09-04 14:26:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
+from transformers import pipeline
+
+### Create a GPT2 generator pipeline
+generator = pipeline('text-generation', model='gpt2')
+
+
+### Generate the answer to the question "Damascus is a" 
+output = generator("Damascus is a", max_length=20, num_return_sequences=10, truncation=True)[0]
+
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    output
 )
