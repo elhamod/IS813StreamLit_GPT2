4339435c95628b7b622a19cc8773a0878b7e954f
elhamod <55500282+elhamod@users.noreply.github.com>
2024-10-24 13:14:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,17 @@ st.title("My Super Awesome GPT!")
 generator = pipeline('text-generation', model='gpt2')
 
 prompt = st.text_input("What is your prompt today?", "Damascus is")
+max_token = st.text_input("What's max number of tokens?", 20)
 
-output = generator(prompt, max_length=20, num_return_sequences=10, truncation=True)[0]
+output = generator(prompt, max_length=max_token, num_return_sequences=1, truncation=True, temperature=0.1)[0]
 
 st.write(
-    output["generated_text"]
+    "Predictable response: " + output["generated_text"]
 )
+
+output = generator(prompt, max_length=max_token, num_return_sequences=1, truncation=True, temperature=1.5)[0]
+
+st.write(
+    "Creative response: " + output["generated_text"]
+)
+
